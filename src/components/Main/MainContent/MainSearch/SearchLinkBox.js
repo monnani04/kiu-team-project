5ca8e7fa45fdc6d55f0ede0e4338f23c7e446c15
yunha <wjh0970@gmail.com>
2022-11-03 02:16:31 +0900

    yunha_search_btn, map add

---
+++ src/components/Main/MainContent/MainSearch/SearchLinkBox.js
@@ -0,0 +1,15 @@
+import style from "../../../../css/Main/MainContent/MainSearch/SearchLinkBox.module.css";
+
+export default function SearchLinkBox() {
+  return (
+    <>
+      <div className={style.box}>
+        <div className={style.textGrp}>
+          <div>여행 계획을 짜보세요!</div>
+          <div>여행 장소 • 날짜 • 인원 수 선택</div>
+        </div>
+        <div className={style.searchImg}></div>
+      </div>
+    </>
+  );
+}
