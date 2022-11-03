c411b7f7c29740fa07e7e80d1588a8c9d58a2af2
minjeong <kmjung528727@gmail.com>
2022-11-03 16:38:14 +0900

    myprofile

--- src/components/Main/MainContent/MainProfile.js
+++ src/components/Main/MainContent/MainProfile.js
@@ -1,10 +1,13 @@
-import Profile from "./MainProfile/ProfileLogin.js";
+import ProfileLogin from "./MainProfile/ProfileLogin.js";
+import Profile from "./MainProfile/Profile.js";
+
 
 
 export default function MainProfile(){
     return(
         <>
-        <Profile/>
+        {/* <ProfileLogin/> */}
+        <Profile></Profile>
         </>
     )
 }
--- src/components/Main/MainContent/MainProfile/ProfileLogin.js
+++ src/components/Main/MainContent/MainProfile/ProfileLogin.js
@@ -1,11 +1,12 @@
 import Login from "../../../Login/Login.js";
-import style from "../../../../css/Main/MainContent/MainProfile/Profile.module.css";
+import style from "../../../../css/Main/MainContent/MainProfile/ProfileLogin.module.css";
 
 
 export default function ProfileLogin() {
     return (<> 
         <div className={style.wrap}>
-            <Login />
+             <Login /> 
+           
         </div>
         </>);
   }
