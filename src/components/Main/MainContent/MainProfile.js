d297e57e48e5bd6b1d115bc2eb76b581f1774df8
minjeong <kmjung528727@gmail.com>
2022-11-03 16:45:52 +0900

    myprofile2

--- src/components/Main/MainContent/MainProfile.js
+++ src/components/Main/MainContent/MainProfile.js
@@ -1,13 +1,18 @@
 import ProfileLogin from "./MainProfile/ProfileLogin.js";
 import Profile from "./MainProfile/Profile.js";
+import { useState } from "react";
 
 
 
 export default function MainProfile(){
+    const [auth, setAuth] = useState(false);
+
+    console.log(auth);
+
     return(
         <>
-        {/* <ProfileLogin/> */}
-        <Profile></Profile>
+        { auth === false ? <ProfileLogin/> : <Profile/>}
+        
         </>
     )
 }
