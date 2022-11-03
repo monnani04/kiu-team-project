1cc99efc2cf71e4526e2310b1325b2bc47cbf065
heejin <gmlwls0394@naver.com>
2022-11-03 16:49:47 +0900

    hee commit3

---
+++ src/components/Main/MainContent/MainWishList/WishItem.js
@@ -0,0 +1,13 @@
+export default function WishItem() {
+    return (
+        <>
+        <div style={{
+            width:"150px",
+            height:"150px",
+            backgroundColor:"gray",
+            margin:"7px",
+            borderRadius:"10px"
+        }}></div>
+        </>
+    )
+}
