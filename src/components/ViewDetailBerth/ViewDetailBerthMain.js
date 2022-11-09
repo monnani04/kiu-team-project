53dfce03712a77ec23babcb2706da1e85fed072c
yunha <wjh0970@gmail.com>
2022-11-09 15:48:18 +0900

    Lamah_app add

--- src/components/ViewDetailBerth/ViewDetailBerthMain.js
+++ src/components/ViewDetailBerth/ViewDetailBerthMain.js
@@ -1,20 +1,26 @@
+import { useEffect } from "react";
 import style from "../../css/ViewDetailBerth/ViewDetailBerthMain.module.css";
 import BackBtn from "./BackBtn";
+import HostInfo from "./HostInfo";
 import TitleInfo from "./TitleInfo";
 import ViewDetailBerthSwiper from "./ViewDetailBerthSwiper";
 import WishBtn from "./WishBtn";
 
 export default function ViewDetailBerthMain(props) {
-    // console.log(props.state);
+  // console.log(props.state);
   const ImgArr = props.state.titleImg;
-//   console.log(ImgArr);
+  //   console.log(ImgArr);
+  useEffect(() => {
+    document.body.style.overflow = "auto";
+  }, []);
   return (
     <>
       <div className={style.wrap}>
-        <BackBtn/>
-        <WishBtn/>
-        <ViewDetailBerthSwiper imgArr = {ImgArr}/>
-        <TitleInfo info = {props.state}/>
+        <BackBtn />
+        <WishBtn />
+        <ViewDetailBerthSwiper imgArr={ImgArr} />
+        <TitleInfo info={props.state} />
+        <HostInfo info={props.state}/>
       </div>
     </>
   );
