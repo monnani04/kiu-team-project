27a9e6c148070c0abe3c8734e6f4053b7af837b5
yunha <wjh0970@gmail.com>
2022-11-19 12:45:42 +0900

    11-19 start

--- src/components/Main/MainContent/MainProfile.js
+++ src/components/Main/MainContent/MainProfile.js
@@ -21,5 +21,15 @@ export default function MainProfile() {
         setAuth(false);
       });
   }, []);
-  return <>{auth === false ? <ProfileLogin /> : auth===true ?<Profile authState={setAuth}/> : false}</>;
+  return (
+    <>
+      {auth === false ? (
+        <ProfileLogin />
+      ) : auth === true ? (
+        <Profile authState={setAuth} />
+      ) : (
+        false
+      )}
+    </>
+  );
 }
